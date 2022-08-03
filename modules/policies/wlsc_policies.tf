da21e07c6db33d025ef2f8fcd734a1d919dda23a
Roberto Sanchez <roberto.s.sanchez@oracle.com>
2022-08-03 13:29:32 -0500

    Address code review comments
    - Use underscore for resources and outputs
    - Run terraform fmt
    - Add validation to some vars
    - Add description to outputs

--- modules/policies/wlsc_policies.tf
+++ modules/policies/wlsc_policies.tf
@@ -1,10 +1,10 @@
 locals {
   # TODO Add other policy statements
   policy_statements = ["Allow dynamic-group ${oci_identity_dynamic_group.wlsc_instance_principal_group[0].name} to manage instance-family in compartment id ${var.compartment_id}"]
-  label_prefix = replace(var.service_name, "/[^a-zA-Z0-9]/", "")
+  label_prefix      = replace(var.service_name, "/[^a-zA-Z0-9]/", "")
 }
 
-resource "oci_identity_policy" "wlsc-oci-policy" {
+resource "oci_identity_policy" "wlsc_oci_policy" {
   count = var.create_policies ? 1 : 0
 
   compartment_id = var.tenancy_id
