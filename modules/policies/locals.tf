8a7f70814f3abce454355fe5af6a95bf0ca795c3
Roberto Sanchez <roberto.s.sanchez@oracle.com>
2022-08-09 12:02:27 -0500

    Code review
    - Add comment to policy

--- modules/policies/locals.tf
+++ modules/policies/locals.tf
@@ -3,6 +3,7 @@ locals {
   label_prefix = replace(var.resource_name_prefix, "/[^a-zA-Z0-9]/", "")
   compartment_rule = format("instance.compartment.id='%s'", var.compartment_id)
 
+  # This policy with "use instances" verb is needed because there is code in the WebLogic for OCI compute image that updates metadata of the compute instance, when more than one VM nodes are created
   core_policy_statement1    = "Allow dynamic-group ${oci_identity_dynamic_group.wlsc_instance_principal_group.name} to use instances in compartment id ${var.compartment_id}"
   core_policy_statement2    = "Allow dynamic-group ${oci_identity_dynamic_group.wlsc_instance_principal_group.name} to inspect volumes in compartment id ${var.compartment_id}"
   core_policy_statement3    = "Allow dynamic-group ${oci_identity_dynamic_group.wlsc_instance_principal_group.name} to inspect volume-attachments in compartment id ${var.compartment_id}"
