27bee068373b160be7f88d5e845756379dae66ab
Sarada Kommalapati <sarada.kommalapati@oracle.com>
2022-08-17 11:29:34 -0700

    Fixed the comments.

--- modules/network/vcn/outputs.tf
+++ modules/network/vcn/outputs.tf
@@ -1,10 +1,10 @@
 # Output of the vcn creation
 output "vcn_id" {
-  description = "The OCID of the new VCN or existing VCN"
+  description = "The OCID of the new VCN"
   value       = oci_core_virtual_network.wls_vcn.id
 }
 
 output "vcn_cidr" {
-  description = "The CIDR value of the new VCN or existing VCN"
-  value       = oci_core_virtual_network.wls_vcn.*.cidr_block
+  description = "The CIDR value of the new VCN"
+  value       = oci_core_virtual_network.wls_vcn.cidr_block
 }
