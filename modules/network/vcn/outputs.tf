83d470152f4761840ba42101ab176dcfab819163
Sarada Kommalapati <sarada.kommalapati@oracle.com>
2022-08-17 10:21:02 -0700

    Fix the comments.

--- modules/network/vcn/outputs.tf
+++ modules/network/vcn/outputs.tf
@@ -1,10 +1,10 @@
 # Output of the vcn creation
 output "vcn_id" {
   description = "The OCID of the new VCN or existing VCN"
-  value       = local.vcn_id
+  value       = oci_core_virtual_network.wls_vcn.id
 }
 
 output "vcn_cidr" {
   description = "The CIDR value of the new VCN or existing VCN"
-  value       = local.vcn_cidr
+  value       = oci_core_virtual_network.wls_vcn.*.cidr_block
 }
--- modules/network/vcn/vcn.tf
+++ modules/network/vcn/vcn.tf
@@ -1,8 +1,4 @@
 resource "oci_core_virtual_network" "wls_vcn" {
-  // If vcn_name is provided and existing subnets are not used then we create VCN regardless of existing vcn_id value.
-  // So vcn_name has preference.
-  // If user wants to use existing vcn_id, then don't provide vcn_name and new VCN won't be created.
-  count = var.vcn_name !="" ? 1:0
 
   cidr_block     = var.wls_vcn_cidr
   dns_label      = format("%svcn",substr((var.resource_name_prefix), 0, 10))
