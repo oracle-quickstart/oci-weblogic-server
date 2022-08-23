1104913f94c0e6f6dbec1a10c7b7565a4c52a52f
Mahuwa Barman <mahuwa.barman@oracle.com>
2022-08-23 18:24:04 +0530

    topic_mbarman_jcs-13184_IDCS_support

--- idcs_variables.tf
+++ idcs_variables.tf
@@ -2,37 +2,43 @@
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 variable "is_idcs_selected" {
-  type = bool
+  type        = bool
   description = "Indicates that idcs has to be provisioned"
-  default = false
+  default     = false
 }
 
 variable "idcs_host" {
+  type        = string
   description = "value for idcs host"
-  default = "identity.oraclecloud.com"
+  default     = "identity.oraclecloud.com"
 }
 
 variable "idcs_port" {
+  type        = number
   description = "value for idcs port"
-  default = "443"
+  default     = 443
 }
 
 variable "idcs_tenant" {
+  type        = string
   description = "value for idcs tenant"
-  default = ""
+  default     = ""
 }
 
 variable "idcs_client_id" {
+  type        = string
   description = "value for idcs client id"
-  default = ""
+  default     = ""
 }
 
 variable "idcs_client_secret_id" {
+  type        = string
   description = "The OCID of the vault secret containing the password for for idcs client secret"
-  default = ""
+  default     = ""
 }
 
 variable "idcs_cloudgate_port" {
+  type        = number
   description = "value for idcs cloud gate port"
-  default = "9999"
+  default     = 9999
 }
