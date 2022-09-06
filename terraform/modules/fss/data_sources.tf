c5b8dbf15dac1870bcf6889285de621a8a33c390
Sarada Kommalapati <sarada.kommalapati@oracle.com>
2022-09-06 04:05:24 -0700

    Minor ffixes.

--- terraform/modules/fss/data_sources.tf
+++ terraform/modules/fss/data_sources.tf
@@ -1,6 +1,11 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+data "oci_core_vcn" "wls_vcn" {
+  #Required
+  vcn_id = var.vcn_id
+}
+
 data "oci_file_storage_mount_targets" "mount_target" {
   #Required
   availability_domain = var.availability_domain
@@ -12,3 +17,4 @@ data "oci_core_private_ip" "mount_target_private_ip" {
   #Required
   private_ip_id = data.oci_file_storage_mount_targets.mount_target.mount_targets[0].private_ip_ids[0]
 }
+
--- terraform/modules/fss/locals.tf
+++ terraform/modules/fss/locals.tf
@@ -1,11 +1,6 @@
 # Copyright (c) 2022, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-data "oci_core_vcn" "wls_vcn" {
-  #Required
-  vcn_id = var.vcn_id
-}
-
 locals {
   anywhere        = "0.0.0.0/0"
   vcn_cidr        = data.oci_core_vcn.wls_vcn.cidr_block
