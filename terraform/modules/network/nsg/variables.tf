957421e7aaa434ac49639ed0aa14573b8189127e
Mahuwa Barman <mahuwa.barman@oracle.com>
2022-09-09 20:07:11 +0530

    Create NSGs for compute, lb, fss and bastion

--- terraform/modules/network/nsg/variables.tf
+++ terraform/modules/network/nsg/variables.tf
@@ -4,30 +4,26 @@
 variable "compartment_id" {
   type        = string
   description = "The OCID of the compartment where the nsg will be created"
-  validation {
-    condition     = length(regexall("^ocid1.compartment.*$", var.compartment_id)) > 0
-    error_message = "WLSC-ERROR: The value for compartment_id should start with \"ocid1.compartment.\"."
-  }
 }
 
 variable "nsg_name" {
-  type = string
+  type        = string
   description = "A user-friendly nsg name"
 }
 
 variable "vcn_id" {
-  type = string
+  type        = string
   description = "The OCID of the VCN to contain the nsg"
 }
 
 variable "tags" {
   type = object({
-    defined_tags    = map(any),
-    freeform_tags   = map(any),
+    defined_tags  = map(any),
+    freeform_tags = map(any),
   })
   description = "Defined tags and freeform tags to be added to the VCN"
   default = {
-    defined_tags    = {},
-    freeform_tags   = {},
+    defined_tags  = {},
+    freeform_tags = {},
   }
 }
