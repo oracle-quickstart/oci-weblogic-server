47a24330315f2c6fd693652fe0372ec420040e29
Mahuwa Barman <106961326+Mahuwa-Barman@users.noreply.github.com>
2022-10-12 10:05:25 +0530

    Remove rule to allow ssh port from internet from the default security list (#59)

--- terraform/modules/network/vcn/vcn.tf
+++ terraform/modules/network/vcn/vcn.tf
@@ -14,3 +14,50 @@ resource "oci_core_virtual_network" "wls_vcn" {
     ignore_changes = [defined_tags, freeform_tags]
   }
 }
+
+resource "oci_core_default_security_list" "default_security_list" {
+  # Default security lists is created with all rules with VCN CIDR as source, instead of 0.0.0.0/0
+  manage_default_resource_id = oci_core_virtual_network.wls_vcn.default_security_list_id
+  egress_security_rules {
+    description      = "Allow all outbound traffic"
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+    stateless        = "false"
+  }
+
+  ingress_security_rules {
+    description = ""
+    icmp_options {
+      code = "4"
+      type = "3"
+    }
+    protocol    = "1"
+    source      = oci_core_virtual_network.wls_vcn.cidr_block
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+
+  ingress_security_rules {
+    description = ""
+    icmp_options {
+      type = "3"
+    }
+    protocol    = "1"
+    source      = oci_core_virtual_network.wls_vcn.cidr_block
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+  }
+
+  ingress_security_rules {
+    description = ""
+    protocol    = "6"
+    source      = oci_core_virtual_network.wls_vcn.cidr_block
+    source_type = "CIDR_BLOCK"
+    stateless   = "false"
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+}
