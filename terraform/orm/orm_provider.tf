6db36e61b16c35835d24bee801954d0de0975bcc
Sarada Kommalapati <sarada.kommalapati@oracle.com>
2022-10-13 11:00:08 -0700

    Removed the versions.

--- terraform/orm/orm_provider.tf
+++ terraform/orm/orm_provider.tf
@@ -2,27 +2,10 @@
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 provider "oci" {
-  version = "4.96.0"
   region           = var.region
 }
 provider "oci" {
-  version = "4.96.0"
   alias                = "home"
   region               = local.home_region
   disable_auto_retries = true
-}
-provider "tls" {
-  version = "~>4.0.3"
-}
-provider "null" {
-  version = "~>3.1.1"
-}
-provider "random" {
-  version = "~>3.4.3"
-}
-provider "template" {
-  version = "~>2.2.0"
-}
-provider "time" {
-  version = "~>0.9.0"
 }
