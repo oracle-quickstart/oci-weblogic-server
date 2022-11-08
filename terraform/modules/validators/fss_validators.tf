872bb7cd32589b5f5e9b5d8c47b35b5d7c2747d2
Sarada Kommalapati <sarada.kommalapati@oracle.com>
2022-11-08 05:54:59 -0800

    Revert "more fss fixes for AD specific subnet."
    This reverts commit 2f5e48d6b8b8c1645e61478e20f4e52785e870bf.

--- terraform/modules/validators/fss_validators.tf
+++ terraform/modules/validators/fss_validators.tf
@@ -7,11 +7,11 @@ locals {
   missing_mnt_compartment_id_msg       = "WLSC-ERROR: The value for mount target compartment id is required if existing mount target is used for file system"
   validate_mount_target_compartment_id = var.mount_target_id != "" && var.mount_target_compartment_id == "" ? local.validators_msg_map[local.missing_mnt_compartment_id_msg] : null
 
-  #missing_mount_target_subnet_cidr_msg      = "WLSC-ERROR: The value for mount target subnet cidr is required if file system is required"
-  #validate_missing_mount_target_subnet_cidr = var.add_fss && var.mount_target_id == "" && var.mount_target_subnet_id == "" && var.mount_target_subnet_cidr == "" ? local.validators_msg_map[local.missing_mount_target_subnet_cidr_msg] : null
+  missing_mount_target_subnet_cidr_msg      = "WLSC-ERROR: The value for mount target subnet cidr is required if file system is required"
+  validate_missing_mount_target_subnet_cidr = var.add_fss && var.mount_target_id == "" && var.mount_target_subnet_id == "" && var.mount_target_subnet_cidr == "" ? local.validators_msg_map[local.missing_mount_target_subnet_cidr_msg] : null
 
-  #missing_mount_target_subnet_id_msg      = "WLSC-ERROR: The value for mount target subnet id is required if existing subnets are required"
-  #validate_missing_mount_target_subnet_id = var.add_fss && var.use_existing_subnets && var.mount_target_subnet_id == "" && var.mount_target_subnet_cidr != "" ? local.validators_msg_map[local.missing_mount_target_subnet_id_msg] : null
+  missing_mount_target_subnet_id_msg      = "WLSC-ERROR: The value for mount target subnet id is required if existing subnets are required"
+  validate_missing_mount_target_subnet_id = var.add_fss && var.use_existing_subnets && var.mount_target_subnet_id == "" && var.mount_target_subnet_cidr != "" ? local.validators_msg_map[local.missing_mount_target_subnet_id_msg] : null
 
   invalid_existing_fss_id_msg = "WLSC-ERROR: The existing file system id should belong to the file system availability domain and compartment that user provides"
   validate_existing_fss_id    = var.add_fss && var.existing_fss_id != "" && (var.fss_availability_domain_existing_fss != var.fss_availability_domain) ? local.validators_msg_map[local.invalid_existing_fss_id_msg] : null
