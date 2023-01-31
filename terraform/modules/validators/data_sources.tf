00738e6ceffb08b9626364254c7cf309f5e817b2
Roberto Sanchez Herrera <roberto.s.sanchez@oracle.com>
2023-01-31 14:12:41 -0600

    JCS-13633 Add licensing section to README.md (#169)
    - Change year in header
    - Add license info in main README.md
    - Fix text in main README.md and README.md for solutions
    - Remove cleanup note for non-jrf solution, because it is not needed
    - Mention the var file for images in the README.md for each solution
    - Improve instructions to create tfvar file

--- terraform/modules/validators/data_sources.tf
+++ terraform/modules/validators/data_sources.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, Oracle and/or its affiliates.
+# Copyright (c) 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 data "oci_core_subnets" "existing_vcn_subnets_data_source" {
