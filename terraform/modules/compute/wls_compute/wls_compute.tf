c1b6b528a28956d4af9a91c5dff9500fe89fa4f9
Roberto Sanchez Herrera <roberto.s.sanchez@oracle.com>
2024-03-21 16:47:24 -0600

    JCS-14392 -  Issue with volume attachments on scale-out
    - Make the keys of the maps of compute and volumes resources to
      have 2 digits at the end, to conserve the iteration order, which
      is lexicographical, to prevent volume attachments from being
      reassigned to other instances because of the iteration order in the
      list of compute instances

--- terraform/modules/compute/wls_compute/wls_compute.tf
+++ terraform/modules/compute/wls_compute/wls_compute.tf
@@ -9,7 +9,7 @@ module "wls-instances" {
 
   source = "../instance"
 
-  instance_params = { for x in range(var.num_vm_instances) : "${local.host_label}-${x}" => {
+  instance_params = { for x in range(var.num_vm_instances) : "${local.host_label}-${format("%02d", x)}" => {
 
     availability_domain = var.use_regional_subnet ? local.ad_names[(x + local.admin_ad_index) % length(local.ad_names)] : var.availability_domain
 
