60ef694cbc852c7f78a8c7a7ea90291dfa826cd6
Sarada Kommalapati <52939928+skommala@users.noreply.github.com>
2024-07-08 09:53:35 -0700

    Development (#276)
    Merge from development to main
    
    ---------
    
    Co-authored-by: Abhijit Paranjpe <109541284+abhijit-paranjpe@users.noreply.github.com>
    Co-authored-by: telake <tim.lake@oracle.com>
    Co-authored-by: Roberto Sanchez Herrera <roberto.s.sanchez@oracle.com>
    Co-authored-by: P M Darshan <p.m.darshan@oracle.com>
    Co-authored-by: C R Aniruddh Srivatsa <c.r.aniruddh.srivatsa@oracle.com>
    Co-authored-by: Mahuwa Barman <106961326+Mahuwa-Barman@users.noreply.github.com>
    Co-authored-by: Chintamani Bhat <chintamani.bhat@oracle.com>
    Co-authored-by: Sarada Kommalapati <sarada.kommlapati@oracle.com>
    Co-authored-by: Adrian Padilla Duarte <adrian.padilla@oracle.com>
    Co-authored-by: Mahuwa-Barman <mahuwa.barman@oracle.com>

--- terraform/modules/validators/variables.tf
+++ terraform/modules/validators/variables.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, 2024 Oracle and/or its affiliates.
+# Copyright (c) 2023, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 variable "compartment_id" {
@@ -534,11 +534,33 @@ variable "tf_script_version" {
   description = "The version of the provisioning scripts located in the OCI image used to create the WebLogic compute instances"
 }
 
-variable "tenancy_id" {
+# All variables under this comment belong to secured production mode
+variable "configure_secure_mode" {
+  type        = bool
+  description = "Set to true to configure a secure WebLogic domain"
+}
+
+variable "keystore_password_id" {
   type        = string
-  description = "The OCID of the tenancy where the dynamic group and policy will be created"
-  validation {
-    condition     = length(regexall("^ocid1.tenancy.*$", var.tenancy_id)) > 0
-    error_message = "WLSC-ERROR: The value for tenancy_id should start with \"ocid1.tenancy.\"."
-  }
+  description = "The OCID of the vault secret with the password for creating the keystore"
+}
+
+variable "root_ca_id" {
+  type        = string
+  description = "The OCID of the existing root certificate authority to issue the certificates"
+}
+
+variable "wls_secondary_admin_password_id" {
+  type        = string
+  description = "The OCID of the vault secret with the password for secondary WebLogic administration user"
+}
+
+variable "administration_port" {
+  type        = number
+  description = "The domain-wide administration port to configure a secure WebLogic domain"
+}
+
+variable "ms_administration_port" {
+  type        = number
+  description = "The administration port for managed servers to configure a secure WebLogic domain"
 }
