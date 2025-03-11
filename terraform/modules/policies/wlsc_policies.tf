ee97bc1715e2730de1ee4efbef2a7fd29259d7a9
telake <tim.lake@oracle.com>
2025-03-11 12:23:43 -0700

    Split policy statements in two due to OCI 50 statement policy limit (#307)
    Split policy statements in two due to OCI 50 statement policy limit.

--- terraform/modules/policies/wlsc_policies.tf
+++ terraform/modules/policies/wlsc_policies.tf
@@ -15,3 +15,19 @@ resource "oci_identity_policy" "wlsc_oci_policy" {
   }
 }
 
+
+resource "oci_identity_policy" "wlsc_oci_policy_autoscaling" {
+
+  count = var.use_autoscaling?1:0
+
+  compartment_id = var.tenancy_id
+  description    = "Autoscaling Policies required for WLS on OCI"
+  name           = "${local.label_prefix}-oci-policy-autoscaling"
+  statements     = local.autoscaling_statements
+
+  defined_tags  = var.tags.defined_tags
+  freeform_tags = var.tags.freeform_tags
+  lifecycle {
+    ignore_changes = [defined_tags, freeform_tags]
+  }
+}
