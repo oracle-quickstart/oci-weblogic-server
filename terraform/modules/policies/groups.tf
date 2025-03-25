4230dbd44c8ad675287a8fad55df124044895f84
Ananya Tripathi <ananya.tripathi@oracle.com>
2025-03-25 08:59:58 +0530

    Code to remove osms from wls for oci stack

--- terraform/modules/policies/groups.tf
+++ terraform/modules/policies/groups.tf
@@ -3,7 +3,7 @@
 
 resource "oci_identity_dynamic_group" "wlsc_instance_principal_group" {
   compartment_id = var.tenancy_id
-  description    = "Dynamic group to allow access to resources with specific tags and allow instances to call osms services"
+  description    = "Dynamic group to allow access to resources with specific tags and allow instances to call oracle cloud agent services"
   matching_rule  = "ALL { ${local.compartment_rule}, ${var.dynamic_group_rule} }"
   name           = "${local.label_prefix}-wlsc-principal-group"
 
