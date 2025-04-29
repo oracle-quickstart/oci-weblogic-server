072ec6157c239a9133e8691bf9f849742ced2c17
ManviGupta-prog <manvi.g.gupta@oracle.com>
2025-04-29 09:35:45 +0530

    Added changes for the variable name

--- terraform/modules/observability/osmh/datasources.tf
+++ terraform/modules/observability/osmh/datasources.tf
@@ -1,6 +1,6 @@
-data "oci_identity_tenancy" "tenancy_ocid" {
+data "oci_identity_tenancy" "tenancy_info" {
     tenancy_id = var.tenancy_id
 }
 data "oci_os_management_hub_software_sources" "all_tenancy_osmh_software_sources" {
-  compartment_id = data.oci_identity_tenancy.tenancy_ocid.id
+  compartment_id = data.oci_identity_tenancy.tenancy_info.id
 }
