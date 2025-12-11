9acc897b9b4ad36fcb86970d4d3e7606c8919f41
Preethi Gnanesh <preethi.gnanesh@oracle.com>
2025-12-11 09:42:06 -0600

    Changing the logic of adding the namespace to ocir_user

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -160,8 +160,7 @@ locals {
   ocir_namespace = data.oci_objectstorage_namespace.object_namespace.namespace
 
   ocir_namespace_with_slash = format("%s/", local.ocir_namespace)
-  ocir_user_starts_with     = substr(var.ocir_user, 0, length(local.ocir_namespace_with_slash))
-  ocir_user                 = local.ocir_user_starts_with == local.ocir_namespace_with_slash ? var.ocir_user : "${format("%s%s", local.ocir_namespace_with_slash, var.ocir_user)}"
+  ocir_user                 = length(regexall("/", var.ocir_user)) > 0 ? var.ocir_user : "${format("%s%s", local.ocir_namespace_with_slash, var.ocir_user)}"
 
   region_keys         = data.oci_identity_regions.all_regions.regions.*.key
   region_names        = data.oci_identity_regions.all_regions.regions.*.name
