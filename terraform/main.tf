f531b86398a31880f6ad57032c0bf0f965f7e4d4
Preethi Gnanesh <preethi.gnanesh@oracle.com>
2026-03-13 10:55:58 -0500

    Verify the changes from IDE

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Copyright (c) 2023, 2025, Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v1.0 as shown at https://oss.oracle.com/licenses/upl.
-
+# Verify for PR from IDE
 ### Removing network validation script from provisioning flow temporarily.
 /*
 module "network-validation" {
